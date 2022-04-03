5309a783474abd5af03b6d0912d8990d2b8fa8c6
Céline Naito <naitoceline@gmail.com>
2022-04-03 16:19:46 +0200

    tried to fic the functions to display message. does not work

--- multiconvert.js
+++ multiconvert.js
@@ -2,22 +2,27 @@
 
 function convertPieds(pieds){
     var pieds= prompt("Entrez une valeur en pieds: ");
-    var result = pieds/ 3.28 ;
-    return result;
+    var result = Number(pieds)/ 3.28 ;
+    var message = "Le résultat est " + result.toString;
+    alert(message);
     
 }
 
 function convertMiles(miles){
     var miles= prompt("Entrez une valeur en miles: ");
-    var result= miles * 1.6 ; 
-    return result;
+    var result= Number(miles) * 1.6 ;
+    var message = "Le résultat est " + result.toString; 
+    alert(message);
     
 }
 
 function convertNoeuds(noeuds){
     var noeuds= prompt("Entrez une valeur en noeuds: ");
-    var result =noeuds * 1.852 ;
-    return result;
+    var result = Number(noeuds) * 1.852 ;
+
+    var message = "Le résultat est " + result.toString;
+    alert(message);
+
     
 }
 // créer un fonction pour démarrer
@@ -34,8 +39,10 @@ function startConvert() {
 //créer une fonction pour choisir la converstion
 function quelleConvert(quelConvert){
     var quelConvert = prompt("Voulez-vous convertir des pieds en mètres (p/m) des miles en km (m/km) des noeuds en kmh (n/kmh)");
+    
     if (quelConvert == "p/m"){
         alert(convertPieds());
+        
     }
     else if (quelConvert == "m/km"){
         alert(convertMiles());
@@ -44,11 +51,11 @@ function quelleConvert(quelConvert){
         alert(convertNoeuds());
     }
     else {
-        console.log("Cette conversion est impossible.")
+        alert("Cette conversion est impossible.");
     }
-    var message = "Le résultat est " + result.toString;
-    alert(message);
-    // added start convert in the function so that you have a loop back to the beginning??
+    
+
+    // added start convert in the function so that you have a loop back to the beginning??// maybe why you have to say no twice.
     startConvert();
 
 }
